6318f4b0cc2c09437170090d9a9a3f7fb5f9ba29
James Silva <contato@inbless.com.br>
2015-07-15 18:10:33 -0300

    Alteração da Rotas para entrega do projeto CRIAÇÃO DE ROTAS

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,22 +11,24 @@
 |
 */
 
-Route::group(['prefix' => 'admin/products'], function(){
-    get('/',['as' => 'products.index', 'uses' =>  'AdminProductsController@index']);
-    get('/create',['as' => 'products.create', 'uses' =>  'AdminProductsController@create']);
-    post('/save',['as' => 'products.save', 'uses' =>  'AdminProductsController@save']);
-    get('/{id}/edit',['as' => 'products.edit', 'uses' =>  'AdminProductsController@edit']);
-    put('/{id}/update',['as' => 'products.update', 'uses' =>  'AdminProductsController@update']);
-    get('/{id}/delete',['as' => 'products.delete', 'uses' =>  'AdminProductsController@delete']);
+Route::group(['prefix' => 'admin'], function(){
+
+    Route::group(['prefix' => 'products'], function(){
+        get('/',['as' => 'products.index', 'uses' =>  'AdminProductsController@index']);
+        get('/create',['as' => 'products.create', 'uses' =>  'AdminProductsController@create']);
+        post('/save',['as' => 'products.save', 'uses' =>  'AdminProductsController@save']);
+        get('/{id}/edit',['as' => 'products.edit', 'uses' =>  'AdminProductsController@edit']);
+        put('/{id}/update',['as' => 'products.update', 'uses' =>  'AdminProductsController@update']);
+        get('/{id}/delete',['as' => 'products.delete', 'uses' =>  'AdminProductsController@delete']);
+    });
+
+    Route::group(['prefix' => 'categories'], function(){
+        get('/',['as' => 'categories.index', 'uses' =>  'AdminCategoriesController@index']);
+        get('/create',['as' => 'categories.create', 'uses' =>  'AdminCategoriesController@create']);
+        post('/save',['as' => 'categories.save', 'uses' =>  'AdminCategoriesController@save']);
+        get('/{id}/edit',['as' => 'categories.edit', 'uses' =>  'AdminCategoriesController@edit']);
+        put('/{id}/update',['as' => 'categories.update', 'uses' =>  'AdminCategoriesController@update']);
+        get('/{id}/delete',['as' => 'categories.delete', 'uses' =>  'AdminCategoriesController@delete']);
+    });
+
 });
-
-
-Route::group(['prefix' => 'admin/categories'], function(){
-    get('/',['as' => 'categories.index', 'uses' =>  'AdminCategoriesController@index']);
-    get('/create',['as' => 'categories.create', 'uses' =>  'AdminCategoriesController@create']);
-    post('/save',['as' => 'categories.save', 'uses' =>  'AdminCategoriesController@save']);
-    get('/{id}/edit',['as' => 'categories.edit', 'uses' =>  'AdminCategoriesController@edit']);
-    put('/{id}/update',['as' => 'categories.update', 'uses' =>  'AdminCategoriesController@update']);
-    get('/{id}/delete',['as' => 'categories.delete', 'uses' =>  'AdminCategoriesController@delete']);
-});
-
