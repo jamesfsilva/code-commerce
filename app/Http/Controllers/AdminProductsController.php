1aae29c0c71600d36a0c4a653da8c0f3318c47f1
James Silva <contato@inbless.com.br>
2015-07-20 18:16:03 -0300

    Corrigido alteração na atualização dos campos de featured e recommended do produto.

--- app/Http/Controllers/AdminProductsController.php
+++ app/Http/Controllers/AdminProductsController.php
@@ -3,8 +3,14 @@
 namespace CodeCommerce\Http\Controllers;
 
 use CodeCommerce\Product;
+use CodeCommerce\Category;
 use CodeCommerce\Http\Requests;
 use CodeCommerce\Http\Controllers\Controller;
+use CodeCommerce\ProductImage;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class AdminProductsController extends Controller
 {
@@ -17,16 +23,20 @@ class AdminProductsController extends Controller
 
     public function index()
     {
-        $products = $this->productModel->all();
+        $products = $this->productModel->paginate(10);
 
         return view('products.index',[
             'products' => $products
         ]);
     }
 
-    public function create()
+    public function create(Category $category)
     {
-        return view('products.create');
+        $categories = $category->lists('name','id');
+        
+        return view('products.create', [
+            'categories' => $categories
+        ]);
     }
 
     public function save(Requests\ProductRequest $request)
@@ -39,18 +49,23 @@ class AdminProductsController extends Controller
         return redirect()->route('products.index');
     }
 
-    public function edit($id)
+    public function edit($id,Category $category)
     {
         $product = $this->productModel->find($id);
+        $categories = $category->lists('name','id');
 
         return view('products.edit', [
-            'product' => $product
+            'product' => $product,
+            'categories' => $categories
         ]);
     }
 
     public function update(Requests\ProductRequest $request, $id)
     {
         $input = $request->all();
+        
+        $input['featured'] = isset($input['featured']) ? true : false;
+        $input['recommend'] = isset($input['recommend']) ? true : false;
 
         $this->productModel->find($id)->update($input);
 
@@ -62,5 +77,38 @@ class AdminProductsController extends Controller
         $this->productModel->find($id)->delete();
 
         return redirect()->route('products.index');
+    }
+    
+    public function images($productId)
+    {
+        $product = $this->productModel->find($productId);
+        
+        return view('products.images.index', [
+            'product' => $product
+        ]);
+    }
+    
+    public function imagesCreate($productId)
+    {
+        $product = $this->productModel->find($productId);
+        
+        return view('products.images.create', [
+            'product' => $product
+        ]);
+    }
+    
+    public function imagesSave(Request $request, $productId, ProductImage $productImage)
+    {
+        $file = $request->file('image');
+        $extension = $file->getClientOriginalExtension();
+        
+        $image = $productImage->create([
+            'product_id' => $productId,
+            'extension' => $extension
+        ]);
+        
+        Storage::disk('local_public')->put($image->id.'.'.$extension, File::get($file));
+
+        return redirect()->route('products.images',['productId'=>$productId]);
     }
 }
